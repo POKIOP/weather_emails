a7c4cc6c66ecfbf44dc2e8c5f9d7a91d7cc4d3e9
pokiop <tomaszjuszczak1@wp.pl>
2023-10-08 14:46:20 -0400

    komit08102023

--- weather_emails/gmail.py
+++ weather_emails/gmail.py
@@ -8,42 +8,35 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from email.message import EmailMessage
 
-EMAIL_CONTENT = 'Today in Warsaw is ... C degrees.'
-EMAIL_RECIPIENT = 'jkstycz91@gmail.com'
+
 SCOPES = ('https://www.googleapis.com/auth/gmail.send',)
-SUBJECT = 'Tempreture today'
+
 TOKEN_FILE_NAME = 'token.json'
 CREDENTIALS_FILE_NAME = 'credentials.json'
 
 
 def get_credentials():
-    creds = None
-    
-    if os.path.exists(TOKEN_FILE_NAME):
-        creds = Credentials.from_authorized_user_file(TOKEN_FILE_NAME, SCOPES)
-    
+    creds = Credentials.from_authorized_user_file(TOKEN_FILE_NAME, SCOPES) if os.path.exists(TOKEN_FILE_NAME) else None
+                
     if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS_FILE_NAME, SCOPES)
-            creds = flow.run_local_server(port=0)
-        
-        with open(TOKEN_FILE_NAME, 'w') as token:
-            token.write(creds.to_json())
-           # print("hello")
-
-
+        flow = InstalledAppFlow.from_client_secrets_file(
+            CREDENTIALS_FILE_NAME, SCOPES)
+        creds = flow.run_local_server(port=0)
+    elif creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    with open(TOKEN_FILE_NAME, 'w') as token:
+        token.write(creds.to_json())
+           
     return creds
 
-def send_email(creds):
+
+def send_email(creds, email_content, email_recipient, subject):
     try:
         service = build('gmail', 'v1', credentials=creds)
         message = EmailMessage()
-        message.set_content(EMAIL_CONTENT)
-        message['To'] = EMAIL_RECIPIENT
-        message['Subject'] = SUBJECT
+        message.set_content(email_content)
+        message['To'] = email_recipient
+        message['Subject'] = subject
         encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
             .decode()
 
@@ -52,9 +45,9 @@ def send_email(creds):
         }
         send_message = (service.users().messages().send
                         (userId="me", body=create_message).execute())
-        print(F'Message Id: {send_message["id"]}')
+        print(f'Message Id: {send_message["id"]}')
     except HttpError as error:
-        print(F'An error occurred: {error}')
+        print(f'An error occurred: {error}')
         send_message = None
     return send_message
 
