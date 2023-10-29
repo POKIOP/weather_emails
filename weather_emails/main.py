cf19af29a94ac358510f53320a7ab1a5f672e4af
pokiop <tomaszjuszczak1@wp.pl>
2023-10-29 13:50:48 -0400

    kommit Oct 29

--- weather_emails/main.py
+++ weather_emails/main.py
@@ -1,5 +1,6 @@
 #polaczyc plik weather i gmail zeby zbieralo informacje i wysylalo emaila
 # wiecej parametrow pogodowych - zmiany w email contetnt
+# https://builtin.com/data-science/sqlite
 
 from weather_emails import gmail, weather
 import sqlite3
@@ -15,22 +16,31 @@ WEATHER_FIELDS = "temperature"
 
 
 def main():
-    with sqlite3.connect(DATABASE_FILE) as con:
-        cur = con.cursor()
-        database.execute_query(con, CREATE_TABLE_QUERRY, cur)
-        database.post_user(con, cur, NAME, CITY_NAME, WEATHER_FIELDS)
 
 
-        
+    conn = sqlite3.connect(DATABASE_FILE)
+    c = conn.cursor()
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users' ''')
+    if c.fetchone()[0]==1 : {
+        print('Table already exists.')
+    }
+    else:
+        with sqlite3.connect(DATABASE_FILE) as con:
+            cur = con.cursor()
+            database.execute_query(con, CREATE_TABLE_QUERRY, cur)
+            database.post_user(con, cur, NAME, CITY_NAME, WEATHER_FIELDS)
 
 
-#     temperature = weather.get_city_temperature(CITY_NAME)
-#     pressure = weather.get_city_pressure(CITY_NAME)
-#     creds = gmail.get_credentials()
-#     email_content= f'Good morning {NAME},\n\nToday at {CITY_NAME} is {temperature} C degrees and {pressure} hPa.'
-#     message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
+
+
+
+    # temperature = weather.get_city_temperature(CITY_NAME)
+    # pressure = weather.get_city_pressure(CITY_NAME)
+    # creds = gmail.get_credentials()
+    # email_content= f'Good morning {NAME},\n\nToday at {CITY_NAME} is {temperature} C degrees and {pressure} hPa.'
+    # message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
     
-#     print(message_id)
+    # print(message_id)
 
 
 
