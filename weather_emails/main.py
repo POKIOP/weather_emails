4cd11e32e9c74fe85d58944fc0784710e3055243
pokiop <tomaszjuszczak1@wp.pl>
2023-10-30 13:29:36 -0400

    komit 30-10

--- weather_emails/main.py
+++ weather_emails/main.py
@@ -6,41 +6,39 @@ from weather_emails import gmail, weather
 import sqlite3
 import database
 
-CITY_NAME = "North Berwick"
+CITY_NAME = "New York"
 DATABASE_FILE = "weather.sqlite"
 EMAIL_RECIPIENT = 'jkstycz91@gmail.com'
-NAME = "John"
+NAME = "James"
 CREATE_TABLE_QUERRY = "CREATE TABLE users (name VARCHAR, city VARCHAR, weather_fields VARCHAR)"
 SUBJECT = 'Temperature today'
 WEATHER_FIELDS = "temperature"
 
 
 def main():
-
-
-    conn = sqlite3.connect(DATABASE_FILE)
-    c = conn.cursor()
-    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users' ''')
-    if c.fetchone()[0]==1 : {
-        print('Table already exists.')
-    }
-    else:
-        with sqlite3.connect(DATABASE_FILE) as con:
-            cur = con.cursor()
-            database.execute_query(con, CREATE_TABLE_QUERRY, cur)
-            database.post_user(con, cur, NAME, CITY_NAME, WEATHER_FIELDS)
-
-
-
-
-
-    # temperature = weather.get_city_temperature(CITY_NAME)
-    # pressure = weather.get_city_pressure(CITY_NAME)
-    # creds = gmail.get_credentials()
-    # email_content= f'Good morning {NAME},\n\nToday at {CITY_NAME} is {temperature} C degrees and {pressure} hPa.'
-    # message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
+    # conn = sqlite3.connect(DATABASE_FILE)
+    # c = conn.cursor()
+    # c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users' ''')
+    # if c.fetchone()[0]==1:
+    #     print('Table already exists.')
     
-    # print(message_id)
+    # else:
+    #     with sqlite3.connect(DATABASE_FILE) as con:
+    #         cur = con.cursor()
+    #         database.execute_query(con, CREATE_TABLE_QUERRY, cur)
+    #         database.post_user(con, cur, NAME, CITY_NAME, WEATHER_FIELDS)
+
+
+
+
+
+    temperature = weather.get_city_temperature(CITY_NAME)
+    pressure = weather.get_city_pressure(CITY_NAME)
+    creds = gmail.get_credentials()
+    email_content= f'Good morning {NAME},\n\nToday at {CITY_NAME} is {temperature} C degrees and {pressure} hPa.'
+    message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
+    
+    print(message_id)
 
 
 
