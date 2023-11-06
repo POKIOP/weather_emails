dfc6538353c618b90693b9f9d05fa4d4e0a372f8
pokiop <tomaszjuszczak1@wp.pl>
2023-11-06 14:13:03 -0500

    komit1 Nov 6

--- weather_emails/database.py
+++ weather_emails/database.py
@@ -1,34 +1,42 @@
 import sqlite3
 
-CREATE_TABLE_QUERRY = "CREATE TABLE users (name VARCHAR, city VARCHAR, weather_fields VARCHAR)"
+CREATE_TABLE_QUERRY = "CREATE TABLE users (name VARCHAR, city VARCHAR, weather_fields VARCHAR, email VARCHAR PRIMARY KEY)"
 
 
 def execute_query(conn, querry, cur):
     cur.execute(querry)
+    response = cur.fetchall()
     conn.commit()
+    return response
 
 def connect_to_database():
     conn = sqlite3.connect('database.sqlite')
     cur = conn.cursor()
     conn.close()
 
-def post_user(conn, cur, name, city, weather_fields):
-    querry = f'INSERT INTO users(name, city, weather_fields) values ("{name}", "{city}", "{weather_fields}")'
+def post_user(conn, cur, name, city, weather_fields, email):
+    querry = f'INSERT INTO users(name, city, weather_fields, email) values ("{name}", "{city}", "{weather_fields}", "{email}")'
     execute_query(conn, querry, cur)
     
 
-def get_user(conn, cur, user_name):
-    querry = f'SELECT * FROM name WHERE name = {user_name}'
-    execute_query(conn, querry, cur)
+def get_user(conn, cur, email):
+    querry = f'SELECT * FROM users WHERE email = "{email}"'
+    user = execute_query(conn, querry, cur)
+    return user[0]
+
+def get_users(conn, cur):
+    querry = f'SELECT * FROM users'
+    users = execute_query(conn, querry, cur)
+    return users
 
 
-def delete_user(conn, cur, user_name):
-    querry = f'DELETE FROM name WHERE name = {user_name}'
+def delete_user(conn, cur, email):
+    querry = f'DELETE FROM users WHERE email = "{email}"'
     execute_query(conn, querry, cur)
 
 
 def patch_user(conn, cur, city_name, current_city_name):
-    querry = f"""UPDATE users SET city = {city_name} WHERE name = {current_city_name} """
+    querry = f"""UPDATE users SET city = "{city_name}" WHERE city = "{current_city_name}" """
     execute_query(conn, querry, cur)
 
 def create_table_if_not_exist(conn, cur):
--- weather_emails/main.py
+++ weather_emails/main.py
@@ -15,43 +15,38 @@ SUBJECT = 'Temperature today'
 
 
 
-# def main():
-#     conn = sqlite3.connect(DATABASE_FILE)
-#     cursor = conn.cursor()
-#     database.create_table_if_not_exist(conn, cursor)
-#     option = ui.get_user_option()
-#     if option == "delete":
-#         user_name = input("Enter user name: ")
-#         database.delete_user(conn, cursor, user_name)
-#     elif option == "create":
-#         user_name = input("Enter user name: ")
-#         city = input("Enter city name: ")
-#         weather_fields = input("Enter weather component: ")
-#         database.post_user(conn, cursor, user_name, city, weather_fields)
-#     elif option == "update":
-#         city_name = input("Enter new city name: ")
-#         current_city_name = input("Enter city name: ")
-#         database.patch_user(conn, cursor, city_name, current_city_name)
-
-    
-def send_email():
+def main():
     conn = sqlite3.connect(DATABASE_FILE)
     cursor = conn.cursor()
-    cursor.execute("""SELECT * from users""")
-    result = cursor.fetchone()
-    NAME = result[0]
-    CITY_NAME = result[1]
-    WEATHER_FIELDS = result[2]
-    temperature = weather.get_city_temperature(CITY_NAME)
-    pressure = weather.get_city_pressure(CITY_NAME)
-    creds = gmail.get_credentials()
-    email_content= f'Good morning {NAME},\n\nToday at {CITY_NAME} is {WEATHER_FIELDS} hPa.'
-    message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
-    
-    print(message_id)
+    database.create_table_if_not_exist(conn, cursor)
+    option = ui.get_user_option()
+    if option == "delete":
+        user_name = input("Enter user name: ")
+        database.delete_user(conn, cursor, user_name)
+    elif option == "create":
+        user_name = input("Enter user name: ")
+        city = input("Enter city name: ")
+        weather_fields = input("Enter weather component: ")
+        email = input("Enter email adress: ")
+        database.post_user(conn, cursor, user_name, city, weather_fields, email)
+    elif option == "update":
+        city_name = input("Enter new city name: ")
+        current_city_name = input("Enter city name: ")
+        database.patch_user(conn, cursor, city_name, current_city_name)
+    # users = database.get_users(conn, cursor)
+    # for user in users:
+        
+    #     tempereture = weather.get_city_temperature(user[1])
+        
+    # # pressure = weather.get_city_pressure(user[1])
+    #     creds = gmail.get_credentials()
+    #     email_content= f'Good morning {user[0]},\n\nToday at {user[1]} is {tempereture} C degrees.'
+    #     message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
+        
+    #     print(message_id)
 
 
 
 
 if __name__ == "__main__":
-    send_email()
+    main()
