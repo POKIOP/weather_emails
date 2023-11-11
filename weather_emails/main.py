f58db04357df1045ea369452fa1d330c5f3c1678
pokiop <tomaszjuszczak1@wp.pl>
2023-11-11 12:26:04 -0500

    komit2 Nov11

--- weather_emails/main.py
+++ weather_emails/main.py
@@ -33,17 +33,17 @@ def main():
         city_name = input("Enter new city name: ")
         current_city_name = input("Enter city name: ")
         database.patch_user(conn, cursor, city_name, current_city_name)
-    # users = database.get_users(conn, cursor)
-    # for user in users:
-        
-    #     tempereture = weather.get_city_temperature(user[1])
-        
-    # # pressure = weather.get_city_pressure(user[1])
-    #     creds = gmail.get_credentials()
-    #     email_content= f'Good morning {user[0]},\n\nToday at {user[1]} is {tempereture} C degrees.'
-    #     message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
-        
-    #     print(message_id)
+    elif option == "send_email":
+        users = database.get_users(conn, cursor)
+        for user in users:
+            tempereture = weather.get_city_temperature(user[1])
+            
+        # pressure = weather.get_city_pressure(user[1])
+            creds = gmail.get_credentials()
+            email_content= f'Good morning {user[0]},\n\nToday at {user[1]} is {tempereture} C degrees.'
+            message_id = gmail.send_email(creds, email_content, EMAIL_RECIPIENT, SUBJECT)
+            
+            print(message_id)
 
 
 
--- weather_emails/ui.py
+++ weather_emails/ui.py
@@ -1,4 +1,4 @@
-OPTIONS = {"1":"delete", "2":"create", "3":"update" }
+OPTIONS = {"1":"delete", "2":"create", "3":"update", "4":"send_email" }
 
 def get_user_option():
     user_choice = input("Enter your option: ")
